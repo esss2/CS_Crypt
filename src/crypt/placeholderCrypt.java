de3d610c0d485b39f19fd0625eaa1b9661d4c7f3
mry2610 <145834324+mry2610@users.noreply.github.com>
2023-10-02 17:21:06 +0200

    encriptado solo texto

--- src/crypt/placeholderCrypt.java
+++ src/crypt/placeholderCrypt.java
@@ -1,4 +1,41 @@
 package crypt;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import java.util.Base64;
+
 public class placeholderCrypt {
+    public static SecretKey generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        return keyGenerator.generateKey();
+    }
+    public static String encryptText(String text, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+    public static String decryptText(String encryptedText, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+    public static void main(String[] args) throws Exception {
+        SecretKey secretKey = generateKey();
+        System.out.println("Generated Key: " + secretKey);
+        String plaintext = "Texto que quieres encriptar";
+        String encryptedText = encryptText(plaintext, secretKey);
+        System.out.println(plaintext);
+        System.out.println("Texto encriptado: " + encryptedText);
+        String decryptedText = decryptText(encryptedText, secretKey);
+        System.out.println("Texto desencriptado: " + decryptedText);
+    }
 }
+
