0878b4bfbedeb12ff0617d28d33c0b5e7c2fd6cb
mry2610 <145834324+mry2610@users.noreply.github.com>
2023-10-02 18:13:10 +0200

    supuesto encriptado

--- src/crypt/placeholderCrypt.java
+++ src/crypt/placeholderCrypt.java
@@ -1,11 +1,10 @@
 package crypt;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
-import javax.crypto.Cipher;
-import java.util.Base64;
+import java.io.*;
+import java.security.SecureRandom;
 
 public class placeholderCrypt {
     public static SecretKey generateKey() throws NoSuchAlgorithmException {
@@ -13,29 +12,58 @@ public class placeholderCrypt {
         keyGenerator.init(128);
         return keyGenerator.generateKey();
     }
-    public static String encryptText(String text, SecretKey secretKey) throws Exception {
+    public static void encryptImage(String inputFile, String outputFile, SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
-        byte[] encryptedBytes = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            cipherOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        inputStream.close();
+        cipherOutputStream.close();
+
+        System.out.println("Imagen encriptada exitosamente.");
     }
-    public static String decryptText(String encryptedText, SecretKey secretKey) throws Exception {
+    public static void decryptImage(String inputFile, String outputFile, SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-        return new String(decryptedBytes, StandardCharsets.UTF_8);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = cipherInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+
+        cipherInputStream.close();
+        outputStream.close();
+
+        System.out.println("Imagen desencriptada exitosamente.");
     }
+
     public static void main(String[] args) throws Exception {
         SecretKey secretKey = generateKey();
         System.out.println("Generated Key: " + secretKey);
-        String plaintext = "Texto que quieres encriptar";
-        String encryptedText = encryptText(plaintext, secretKey);
-        System.out.println(plaintext);
-        System.out.println("Texto encriptado: " + encryptedText);
-        String decryptedText = decryptText(encryptedText, secretKey);
-        System.out.println("Texto desencriptado: " + decryptedText);
+        String inputFile1 ="C:\\Users\\mgeg2\\IdeaProjects\\CS_Crypt\\src\\crypt\\Tema2-DISM-1.pdf"; // Reemplaza con la ruta de tu imagen de entrada
+        String Encrypt ="C:\\Users\\mgeg2\\IdeaProjects\\CS_Crypt\\src\\crypt\\Tema2-DISM-1.pdf";  // Reemplaza con la ruta de tu imagen encriptada;
+        String inputFile2 ="C:\\Users\\mgeg2\\IdeaProjects\\CS_Crypt\\src\\crypt\\Tema5-DISM-swagger.pdf"; // Reemplaza con la ruta de tu imagen de entrada
+        encryptImage(inputFile1,Encrypt,secretKey);
+
+
     }
 }
 
