467e4bbab4a7b07ab33faa831b37bc3e3647009d
joseecm7 <josecanx.jm@gmail.com>
2023-10-07 12:46:13 +0200

    Primera versión desencriptar

--- src/crypt/placeholderCrypt.java
+++ src/crypt/placeholderCrypt.java
@@ -1,8 +1,70 @@
 package crypt;
 
-public class placeholderCrypt {
+import javax.crypto.*;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-    public void desencriptar(){
+public class placeholderCrypt {
+    public static SecretKey generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        return keyGenerator.generateKey();
+    }
+    public static void encryptImage(String inputFile, String outputFile, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            cipherOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        inputStream.close();
+        cipherOutputStream.close();
+
+        System.out.println("Imagen encriptada exitosamente.");
+    }
+    /*public static void decryptImage(String inputFile, String outputFile, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = cipherInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+
+        cipherInputStream.close();
+        outputStream.close();
+
+        System.out.println("Imagen desencriptada exitosamente.");
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        SecretKey secretKey = generateKey();
+        System.out.println("Generated Key: " + secretKey);
+        String inputFile1 = "C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\src\\crypt\\hola.txt"; // Reemplaza con la ruta de tu imagen de entrada
+        String Encrypt ="C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\src\\crypt\\adios.txt";  // Reemplaza con la ruta de tu imagen encriptada;
+        //String Decrypt ="C:\\Users\\mgeg2\\IdeaProjects\\CS_Crypt\\src\\crypt\\qtal.txt";  // Reemplaza con la ruta de tu imagen encriptada;
+        encryptImage(inputFile1,Encrypt,secretKey);
+        System.out.println(Base64.getEncoder().encodeToString(secretKey.getEncoded()));
+        //decryptImage(Encrypt,Decrypt,secretKey);
 
     }
 }
--- src/decrypt/placeholderDecrypt.java
+++ src/decrypt/placeholderDecrypt.java
@@ -1,4 +1,48 @@
 package decrypt;
 
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.io.*;
+
 public class placeholderDecrypt {
+
+    public static void desencriptar(String claveBase64) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        byte[] claveEnBytes = Base64.getDecoder().decode(claveBase64);
+        SecretKey secretKey = new SecretKeySpec(claveEnBytes, "AES"); // "AES" es el algoritmo que se usará, puedes cambiarlo según tus necesidades
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\src\\crypt\\adios.txt");
+        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\DecryptedFiles\\quetal.txt");
+
+        CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = cipherInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+
+        cipherInputStream.close();
+        outputStream.close();
+
+        System.out.println("Imagen desencriptada exitosamente.");
+    }
+
+    public static void main(String[] args) throws Exception {
+        String claveBase64 = "SYqZtJjcIYapU+oAd2WCcQ=="; // Clave en formato Base64
+        String textoEncriptado = "UBpW4xGpoXQpMwy079TZhOsNlk7TX4lQiO+UTmybvkHAD8A5bX0C+ooNNX6VzH9TiC6xzAsToD5QFcowmYZydg";
+
+        try {
+            desencriptar(claveBase64);
+            System.out.println("Texto encriptado: " + textoEncriptado);
+            //System.out.println("Texto desencriptado: " + textoDesencriptado);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
