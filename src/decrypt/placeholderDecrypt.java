d69980a41aa1c6bef0345ed087a2a82321aeb08e
joseecm7 <josecanx.jm@gmail.com>
2023-10-09 17:42:31 +0200

    Desencriptado con rutas relativas

--- src/decrypt/placeholderDecrypt.java
+++ src/decrypt/placeholderDecrypt.java
@@ -8,15 +8,18 @@ import java.io.*;
 
 public class placeholderDecrypt {
 
-    public static void desencriptar(String claveBase64) throws Exception {
+    public static void desencriptar(String claveBase64, String archivo) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         byte[] claveEnBytes = Base64.getDecoder().decode(claveBase64);
         SecretKey secretKey = new SecretKeySpec(claveEnBytes, "AES"); // "AES" es el algoritmo que se usará, puedes cambiarlo según tus necesidades
 
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-        FileInputStream inputStream = new FileInputStream("C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\src\\crypt\\adios.txt");
-        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\josec\\IdeaProjects\\CS_Crypt\\DecryptedFiles\\quetal.txt");
+
+        FileInputStream inputStream = new FileInputStream("../EncryptedFiles/" + archivo);
+        FileOutputStream outputStream = new FileOutputStream("../DecryptedFiles/" +archivo);
+
+
 
         CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
 
@@ -30,17 +33,14 @@ public class placeholderDecrypt {
         cipherInputStream.close();
         outputStream.close();
 
-        System.out.println("Imagen desencriptada exitosamente.");
+        System.out.println("Archivo desencriptado exitosamente.");
     }
 
     public static void main(String[] args) throws Exception {
         String claveBase64 = "SYqZtJjcIYapU+oAd2WCcQ=="; // Clave en formato Base64
-        String textoEncriptado = "UBpW4xGpoXQpMwy079TZhOsNlk7TX4lQiO+UTmybvkHAD8A5bX0C+ooNNX6VzH9TiC6xzAsToD5QFcowmYZydg";
-
+        String archivoEncriptado = "adios.txt";
         try {
-            desencriptar(claveBase64);
-            System.out.println("Texto encriptado: " + textoEncriptado);
-            //System.out.println("Texto desencriptado: " + textoDesencriptado);
+            desencriptar(claveBase64,archivoEncriptado);
         } catch (Exception e) {
             e.printStackTrace();
         }
